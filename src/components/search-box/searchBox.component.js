8a761044f6d6250d9a1966cfb8cd261119810895
petros_ <p.kontakis@athlenda.com>
2019-06-16 14:19:44 +0300

    IMPOVEMENT / delay on search  input

--- src/components/search-box/searchBox.component.js
+++ src/components/search-box/searchBox.component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+
+const DELAY = 600;
+
 /**
  * Name: SearchBox
  * Description: Search box is responsible to handle input changes
@@ -10,24 +13,39 @@ class SearchBox extends Component {
         searchQuery: ""
     }
 
+    timer;
 
-    
-    handleOnChange = (e) => {
-        this.setState({
-            searchQuery: e.target.value
-        })
-        this.props.onQueryChange(e.target.value);
+    handleSubmit = (e) => {
+        e.preventDefault();
+        clearTimeout(this.timer);
+        this.notifyOnQueryChange();
     }
 
-    render(){
-        const {searchQuery} = this.state;
+    handleOnChange = (e) => {
+
+        clearTimeout(this.timer);
+
+        this.setState({
+            searchQuery: e.target.value
+        }, () => this.timer = setTimeout(this.notifyOnQueryChange, DELAY))
+    }
+
+    notifyOnQueryChange = () => {
+        this.props.onQueryChange(this.state.searchQuery);
+
+    }
+
+    render() {
+        const { searchQuery } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
                 <label htmlFor="search">Search</label>
-                <input onChange={this.handleOnChange} 
-                    type="text" 
-                    id="searchQuery" 
-                    value={searchQuery}/>
+                <input
+                    autoComplete="off"
+                    onChange={this.handleOnChange}
+                    type="text"
+                    id="searchQuery"
+                    value={searchQuery} />
             </form>
         )
     }
