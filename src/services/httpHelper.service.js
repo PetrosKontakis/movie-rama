a98816861183e54f19012a35de85d4005315a0e8
petros_ <p.kontakis@athlenda.com>
2019-06-16 13:49:47 +0300

    Implement cancelers

--- src/services/httpHelper.service.js
+++ src/services/httpHelper.service.js
@@ -1,28 +1,59 @@
 import { THE_MOVIE_DB_API_KEY, API_ROOT } from './config';
 
+
+let httpAborts = [];
+
 /**
  * TODO: Comments
  * @param {*} params 
  */
-const getParamsStringArray = (params) => {
+const getParamsAsString = (params) => {
     return Object.keys(params).map(function (key, value) {
         return `${key}=${params[key]}`
-    });
+    }).join("&");
 }
 
+/**
+ * 
+ */
+const abortCancelers = () => {
+    // Cancel all calls including signal canceler 
+    httpAborts.map(httpAbort => httpAbort.abort());
+}
+
+/**
+ * @return signal
+ */
+const addNewCanceler = () => {
+    //  Create new canceler 
+    const newHttpAbort = new AbortController();
+    // Add a new on to stack 
+    httpAborts.push(newHttpAbort);
+
+    return newHttpAbort.signal;
+}
 
 /**
  * TODO:comments
  */
-const genericHttpCall = (endPoint, params = {}) => {
+const genericHttpCall = (endPoint, params = {}, options = {}) => {
 
+    let fetchOpt = {};
+    if (options.includeCanceler) {
+        // Abort all http calls with includeCanceler
+        abortCancelers();
+        // Add new canceler 
+        const signal = addNewCanceler();
+        //  add signal to fetch opts 
+        fetchOpt['signal'] = signal;
+    }
+
+    // Add parameter api_key
     params['api_key'] = THE_MOVIE_DB_API_KEY;
+    // Convert object parameters to string
+    const urlParameters = getParamsAsString(params);
 
-    const paramsArray = getParamsStringArray(params);
-
-    const paramsString = paramsArray.join("&");
-
-    return fetch(`${API_ROOT}${endPoint}?${paramsString}`)
+    return fetch(`${API_ROOT}${endPoint}?${urlParameters}`, fetchOpt)
         .then(response => response.json())
         .catch(error => error.json());
 
