92c52d1e6e3ee43e6b3264fb5500544770b4b682
petros_ <p.kontakis@athlenda.com>
2019-06-20 00:41:43 +0300

    Updates

---
+++ precache-manifest.539bc82b218c9c153679f79260ab7593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdcaca3e408b7828d6ccc29a86cb3ec",
+    "url": "/movie-rama/index.html"
+  },
+  {
+    "revision": "6f116f779888d5baaab3",
+    "url": "/movie-rama/static/css/main.02e73809.chunk.css"
+  },
+  {
+    "revision": "9b76126fa4ef3a4c6e94",
+    "url": "/movie-rama/static/js/2.40de70b7.chunk.js"
+  },
+  {
+    "revision": "6f116f779888d5baaab3",
+    "url": "/movie-rama/static/js/main.1c5b2b9d.chunk.js"
+  },
+  {
+    "revision": "68c4d7c1324185169310",
+    "url": "/movie-rama/static/js/runtime~main.6ced12e2.js"
+  },
+  {
+    "revision": "5434740cfd131adef8acbdf8fb43ba3a",
+    "url": "/movie-rama/static/media/baseline-keyboard_arrow_left-24px.5434740c.svg"
+  },
+  {
+    "revision": "69e0d64524cd896aafb3f0012b516ad4",
+    "url": "/movie-rama/static/media/baseline-play_circle_filled-24px.69e0d645.svg"
+  }
+]);
